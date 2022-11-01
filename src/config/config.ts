2c6038de6c6f96a2bad6400f0cfc59940a9402d1
DESKTOP-8F9VMMN\hnJin <knsan189@gmail.com>
2022-11-01 09:16:40 +0900

    env 설정 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,10 @@
-export const clientId = "1032458721781100676";
-export const secretId = "NOwvrKTfAwqGth4bk53hIW-50t5FRslD";
-export const publicKey =
-  "b69211376a19be8d134de0f9e1143285ad488aed7732f9e330cd2aa446c95973";
+import dotenv from "dotenv";
 
-export const token =
-  "MTAzMjQ1ODcyMTc4MTEwMDY3Ng.Gfmsj-.diRvoMRAUjyBAT39RkH3QcvI518zeCBGnnG0oA";
+dotenv.config();
+
+export const clientId = `${process.env.DISCORD_CLIENT_ID}`;
+export const secretId = `${process.env.DISCORD_SECRET_ID}`;
+export const publicKey = `${process.env.DISCORD_PUBLIC_KEY}`;
+export const token = `${process.env.DISCORD_TOKEN}`;
+
 export default {};
