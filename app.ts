da5a47416091545f388b5d4245b6346af5780368
DESKTOP-8F9VMMN\hnJin <knsan189@gmail.com>
2022-11-04 15:34:39 +0900

    sync api 연결. logger 추가

--- app.ts
+++ app.ts
@@ -1,9 +1,8 @@
-import { Client, GatewayIntentBits, Events, Collection } from "discord.js";
+import { Client, GatewayIntentBits, Events } from "discord.js";
 import axios from "axios";
-import path from "path";
-import fs from "fs";
-import { clientId, publicKey, token } from "./src/config/config.js";
-import { VoiceLog } from "./src/@types/types.js";
+import { token } from "./src/config/config.js";
+
+let ready = false;
 
 const client = new Client({
   intents: [
@@ -16,32 +15,42 @@ const client = new Client({
   ],
 });
 
-// client.commands = new Collection();
+function syncServer() {
+  if (!ready) return;
+  console.log(`[${new Date().toLocaleString()}] 서버 동기화`);
+  const guilds = client.guilds.cache;
+  const starlight = guilds.get("949451590568398889");
+  const currentMembers = [];
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const commandsPath = path.join(__dirname, "src/commands");
-// const commandFiles = fs
-//   .readdirSync(commandsPath)
-//   .filter((file) => file.endsWith(".ts"));
+  starlight.members.cache.forEach((member) => {
+    if (member.voice.channel) {
+      if (
+        !member.presence?.clientStatus.mobile ||
+        member.presence.clientStatus.desktop
+      ) {
+        currentMembers.push(member.nickname);
+      }
+    }
+  });
 
-// for (const file of commandFiles) {
-//   const filePath = path.join(commandsPath, file);
-//   const command = import(filePath);
-//   // Set a new item in the Collection with the key as the command name and the value as the exported module
+  console.log(
+    `[${new Date().toLocaleString()}] 현재 접속중 사용자 :`,
+    currentMembers.toString(),
+    `(총 ${currentMembers.length}명)`
+  );
 
-//   console.log(file);
-//   if ("data" in command && "execute" in command) {
-//     client.commands.set(command.data.name, command);
-//   } else {
-//     console.log(
-//       `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-//     );
-//   }
-// }
+  axios({
+    method: "POST",
+    url: "http://knsan189.iptime.org:8080/api/history/sync",
+    data: { nicknames: currentMembers },
+  });
+}
 
-client.once(Events.ClientReady, (stream) => {
-  console.log(`Ready! Logged in as ${stream.user.tag}`);
+client.once(Events.ClientReady, async (stream) => {
+  console.log(`[${new Date().toLocaleString()}] ${stream.user.tag} 로그인`);
+  ready = true;
+  syncServer();
+  setInterval(syncServer, 1000 * 60 * 5);
 });
 
 client.on("messageCreate", async (message) => {
@@ -80,12 +89,12 @@ client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     }
 
     if (oldState.channelId === newState.channelId) {
-      console.log(`${nickname} moved channel`);
+      console.log(`${nickname} moved channel`, presence.clientStatus);
       return;
     }
 
     if (oldState.channelId === null) {
-      console.log(`${nickname} joined!`);
+      console.log(`${nickname} joined!`, presence.clientStatus);
       const request = { time: new Date(), nickname, type: "join" };
       await axios({
         method: "POST",
@@ -96,7 +105,7 @@ client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     }
 
     if (newState.channelId === null) {
-      console.log(`${nickname} left!`);
+      console.log(`${nickname} left!`, presence.clientStatus);
       const request = { time: new Date(), nickname, type: "leave" };
       await axios({
         method: "POST",
@@ -107,14 +116,12 @@ client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     }
 
     if (newState.channelId !== oldState.channelId) {
-      console.log(`${nickname} switched channels`);
+      console.log(`${nickname} switched channels`, presence.clientStatus);
     }
   } catch (error) {
     console.log(error);
   }
 });
-
-client.on(Events.Debug, (message) => console.log(message));
 
 client.on(Events.InteractionCreate, (interaction) => {
   console.log(interaction);
